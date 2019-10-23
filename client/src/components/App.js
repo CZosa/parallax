e5c7f62403ab0d140a1b45a98723159d8943468a
Chris Zosa <zosachris@gmail.com>
2019-10-23 14:44:41 -0700

    importing css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/app.css';
 
 class App extends React.Component {
   render() { 
     return (  
-      <div>
+      <div className="container">
         App
       </div>
     );
